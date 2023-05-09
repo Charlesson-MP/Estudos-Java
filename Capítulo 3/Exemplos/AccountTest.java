5e627d900138f4d56c7934a2d1e8c296fc7ecfc2
Charlesson-MP <charlessonmendes@gmail.com>
2023-05-09 11:43:33 -0300

    Customizando a classe AccountTest para usar os novos recursos da classe Account.

--- Capítulo 3/Exemplos/AccountTest.java
+++ Capítulo 3/Exemplos/AccountTest.java
@@ -3,23 +3,62 @@
  * construtor.
  */
 
+import java.util.Scanner; //Importando a classe Scanner da Java API.
+
 public class AccountTest { //Início da classe AccountTest.
     
     //Método main, inicia a execução da aplicação.
     public static void main(String[] args) { //Início do método main.
 
-        Account account1 = new Account("Jane Green"); //Cria um objeto Account com a string Jane Green e o atribui a variável account1.
-        Account account2 = new Account("John Blue"); //Cria um objeto Account com a string John Blue e o atribui a variável account2.
+        Scanner input = new Scanner(System.in); //Cria objeto Scanner para entrada de dados a partir da janela de comando
 
-        System.out.println("___________________________________"); //Exibe um traço horizontal na janela de comando.
+        Account account1 = new Account("Jane Green", 50.00); //Cria um objeto Account com a string Jane Green e o atribui a variável account1.
+        Account account2 = new Account("John Blue", -7.53); //Cria um objeto Account com a string John Blue e o atribui a variável account2.
 
-        System.out.printf("| account1 name is: %s%n", account1.getName()); //Exibe o novo nome armazenado no objeto account1.
+        System.out.println("__________________________________________"); //Exibe um traço horizontal na janela de comando.
 
-        System.out.println("|__________________________________"); //Exibe um traço vertical e outro horizontal na janela de comando.
+        System.out.printf("| %s balance: $%.2f %n", account1.getName(), account1.getBalance()); //Exibe o nome e o saldo armazenado no objeto account1.
 
-        System.out.printf("| account2 name is: %s%n", account2.getName()); //Exibe o novo nome armazenado no objeto account2.
+        System.out.println("|_________________________________________"); //Exibe um traço vertical e outro horizontal na janela de comando.
 
-        System.out.println("|__________________________________"); //Exibe um traço vertical e outro horizontal na janela de comando.
+        System.out.printf("| %s balance: %.2f %n", account2.getName(), account2.getBalance()); //Exibe o nome e o saldo armazenado no objeto account2.
+
+        System.out.println("|_________________________________________"); //Exibe um traço vertical e outro horizontal na janela de comando.
+
+
+        System.out.print("| Enter deposit amount for account1: "); //Mensagem de instrução para o usuário.
+        double depositAmount = input.nextDouble(); //Variável de instância balance do objeto account1 recebe o valor digitado.
+        System.out.printf("|%n| Adding %.2f to account1 balance.%n", depositAmount); //Mensagem ao usuário.
+        account1.deposit(depositAmount); //Adicionando o inserido valor ao objeto account1.
+
+        System.out.println("|_________________________________________"); //Exibe um traço vertical e outro horizontal na janela de comando.
+
+        System.out.printf("| %s balance: $%.2f %n", account1.getName(), account1.getBalance()); //Exibe o nome e o saldo armazenado no objeto account1.
+
+        System.out.println("|_________________________________________"); //Exibe um traço vertical e outro horizontal na janela de comando.
+
+        System.out.printf("| %s balance: %.2f %n", account2.getName(), account2.getBalance()); //Exibe o nome e o saldo armazenado no objeto account2.
+
+        System.out.println("|_________________________________________"); //Exibe um traço vertical e outro horizontal na janela de comando.
+
+
+        System.out.print("| Enter deposit amount for account2: "); //Mensagem de instrução para o usuário.
+        depositAmount = input.nextDouble(); //Variável de instância balance do objeto account1 recebe o valor digitado.
+        System.out.printf("|%n| Adding %.2f to account2 balance.%n", depositAmount); //Mensagem ao usuário.
+        account2.deposit(depositAmount); //Adicionando o inserido valor ao objeto account1.
+
+        System.out.println("|_________________________________________"); //Exibe um traço vertical e outro horizontal na janela de comando.
+
+        System.out.printf("| %s balance: $%.2f %n", account1.getName(), account1.getBalance()); //Exibe o nome e o saldo armazenado no objeto account1.
+
+        System.out.println("|_________________________________________"); //Exibe um traço vertical e outro horizontal na janela de comando.
+
+        System.out.printf("| %s balance: %.2f %n", account2.getName(), account2.getBalance()); //Exibe o nome e o saldo armazenado no objeto account2.
+
+        System.out.println("|_________________________________________"); //Exibe um traço vertical e outro horizontal na janela de comando.
+
+
+        input.close(); //Fecha objeto input.
 
     } //Fim do método main.
 
